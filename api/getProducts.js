dff86914f18a21d241ffefe9c77cd9bbb9792c59
digitalhouse <digitalhouse@MacBook-Pro-de-digitalhouse.local>
2022-07-09 00:43:20 -0300

    comandos tests

--- api/getProducts.js
+++ api/getProducts.js
@@ -2,6 +2,8 @@ const router = require('nordic/ragnar').router();
 /**
  * Aquí deberás crear el endpoint con el método GET, el cuál consuma
  * el servicio que devuelve los productos de la API de MeLi.
+ * 
+ * Comando para correr el test: `npm run test:unit:watch getProducts`
  */
 
 module.exports = router;
--- services/productsService.js
+++ services/productsService.js
@@ -2,6 +2,8 @@
  * Implementar un servicio que tenga un método estático llamado `getProducts`,
  * el cual tiene recibe dos parámetros: siteId y el nombre nombre de un producto.
  * Restclient va a hacer una request a la ruta `/sites/${siteId}/search`.
+ * 
+ * Comando para correr el test: `npm run test:unit:watch productsService`
  */
 const restclient = require('nordic/restclient')({
   timeout: 5000,
