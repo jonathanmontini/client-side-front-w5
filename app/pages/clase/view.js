fb37955793ebb07976aa747ca235792eaf464ee4
jonathanmontini <jonathan.montini@mercadolibre.com>
2022-08-03 13:47:03 -0300

    Practica Nordic Server Client

--- app/pages/clase/view.js
+++ app/pages/clase/view.js
@@ -1,3 +1,23 @@
 const React = require("react");
 const Script = require("nordic/script");
-const serialize = require("ser");
+const serialize = require("serialize-javascript");
+
+function View(props) {
+  const { message } = props;
+
+  return (
+    <>
+      <Script>
+        {`
+                window.__PRELOADED_STATE__ = ${serialize(preloadedState, {
+                  isJSON: true,
+                })}
+                console.log('%cClase page is loaded!', 'color:green')
+            `}
+      </Script>
+      <Script src="vendor.js" />
+      <Script src="clase.js" />
+      <h2>{message}</h2>
+    </>
+  );
+}
